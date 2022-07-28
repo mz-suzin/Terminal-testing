8ac52b939b4445519ca48a8c82cf87b90aef4569
mz-suzin <mzsuzin@gmail.com>
2022-07-28 15:15:34 -0300

    second commit after updating file via git pull

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ let j = [1,2];
 j.forEach(e => console.log(e));
 
 console.log("I'm changing this after the first commit so I can update my local repository using the terminal");
+
+console.log("Now I'm updating it again after receiveing the updated version via git pull");
