d48e4920420627aa238d58f85b41d16014318d09
mz-suzin <mzsuzin@gmail.com>
2022-07-28 16:02:07 -0300

    Commiting the change to a different branch now

--- script.js
+++ script.js
@@ -4,4 +4,6 @@ j.forEach(e => console.log(e));
 
 console.log("I'm changing this after the first commit so I can update my local repository using the terminal");
 
-console.log("Now I'm updating it again after receiveing the updated version via git pull");
+console.log("Writing over the last message so I can see the change and also pushing it to a different branch");
+
+
