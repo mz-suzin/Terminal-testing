238651b744a7319457cd4ea5ba545fb2baa08c65
mz-suzin <mzsuzin@gmail.com>
2022-07-28 16:27:26 -0300

    just a test again

--- script.js
+++ script.js
@@ -5,5 +5,3 @@ j.forEach(e => console.log(e));
 console.log("I'm changing this after the first commit so I can update my local repository using the terminal");
 
 console.log("Writing over the last message so I can see the change and also pushing it to a different branch");
-
-
